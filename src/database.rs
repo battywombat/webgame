5a69f5359c3358a1577240c67689c7505bddab7f
Paul Warner <paul.eliot.warner@gmail.com>
2018-09-02 16:14:33 +0530

    Can now get a page for each character in database.

--- src/database.rs
+++ src/database.rs
@@ -39,17 +39,54 @@ fn create_connection_with_scripts<P>(database: P, schema: Option<P>, testdata: O
     Ok(Mutex::new(conn))
 }
 
-pub fn create_connection<P>(database: P) -> Result<DbConn>
-    where P: AsRef<Path> {
-    create_connection_with_scripts(database, None, None)
-}
+// pub fn create_connection<P>(database: P) -> Result<DbConn>
+//     where P: AsRef<Path> {
+//     create_connection_with_scripts(database, None, None)
+// }
 
-pub fn create_connection_with_schema<P>(database: P, schema: P) -> Result<DbConn>
-    where P: AsRef<Path> {
-    create_connection_with_scripts(database, Some(schema), None)
-}
+// pub fn create_connection_with_schema<P>(database: P, schema: P) -> Result<DbConn>
+//     where P: AsRef<Path> {
+//     create_connection_with_scripts(database, Some(schema), None)
+// }
 
 pub fn create_connection_with_testdata<P>(database: P, schema: P, testdata: P) -> Result<DbConn>
     where P: AsRef<Path> {
     create_connection_with_scripts(database, Some(schema), Some(testdata))
+}
+
+#[derive(Debug)]
+pub struct Character {
+    pub id: i32,
+    pub name: String,
+    pub strength: u8,
+    pub magic: u8,
+    pub vitality: u8,
+    pub agility: u8,
+    pub luck: u8
+}
+
+pub fn get_character(conn: &Connection, id: i32) -> Result<Character> {
+    let res = conn.query_row("SELECT id, cname, strength, magic, vitality, agility, luck FROM characters WHERE id=?", &[&id], |row| {
+        let id = row.get_checked(0)?;
+        let name = row.get_checked(1)?;
+        let strength = row.get_checked(2)?;
+        let magic = row.get_checked(3)?;
+        let vitality = row.get_checked(4)?;
+        let agility = row.get_checked(5)?;
+        let luck = row.get_checked(6)?;
+        Ok(Character {
+            id,
+            name,
+            strength,
+            magic,
+            vitality,
+            agility,
+            luck
+        })
+    });
+
+    match res {
+        Ok(s) => s,
+        Err(e) => Err(e)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate rusqlite;
 mod database;
 
 use std::collections::HashMap;
 use rocket::State;
-use rocket_contrib::Template::DbConn;
-use database
+use rocket_contrib::Template;
+use database::DbConn;
 
 #[get("/")]
 fn index(_db_conn: State<DbConn>) -> Template {
@@ -19,12 +20,32 @@ fn index(_db_conn: State<DbConn>) -> Template {
 
 #[get("/character/<id>")]
 fn get_character(db_conn: State<DbConn>, id: i32) -> Template {
+    let conn = db_conn.lock().unwrap();
     let mut map = HashMap::new();
+    let character = match database::get_character(&conn, id) {
+        Ok(c) => c,
+        Err(rusqlite::Error::QueryReturnedNoRows) => {
+            return Template::render("404", map);
+        }
+        Err(e) => {
+            map.insert("errormessage", format!("{:?}", e));
+            return Template::render("500", map);
+        }
+    };
+    println!("{:?}", character);
+    map.insert("title", format!("{}'s Character Sheet", character.name));
+    map.insert("name", character.name);
+    map.insert("strength", character.strength.to_string());
+    map.insert("magic", character.magic.to_string());
+    map.insert("vitality", character.vitality.to_string());
+    map.insert("agility", character.agility.to_string());
+    map.insert("luck", character.luck.to_string());
+    Template::render("character", map)
 }
 
 fn main() {
     let conn = database::create_connection_with_testdata(":memory:", "schema.sql", "testdata.sql").expect("Failed to open database");
-    rocket::ignite().mount("/", routes![index])
+    rocket::ignite().mount("/", routes![index, get_character])
                     .attach(Template::fairing())
                     .manage(conn)
                     .launch();
