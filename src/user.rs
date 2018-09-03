67b3597ec6424fd33a51de247af0fa22f4d035f4
Paul Warner <paul.eliot.warner@gmail.com>
2018-09-03 17:33:42 +0530

    Added user session storage and logout.

--- src/user.rs
+++ src/user.rs
@@ -1,11 +1,13 @@
 use std;
+use rusqlite;
 
 use std::collections::HashMap;
 use std::fmt::{Formatter, Display};
 use rocket::State;
 use rocket::http::{Cookie, Cookies};
-use rocket::request::Form;
+use rocket::request::{self, FromRequest, Request, FlashMessage, Form};
 use rocket::response::{Redirect, Flash};
+use rocket::outcome::IntoOutcome;
 use rocket_contrib::Template;
 use rusqlite::Connection;
 use database::DbConn;
@@ -16,17 +18,38 @@ pub struct User {
     pub password: String
 }
 
-#[get("/login")]
-pub fn login_page(_db_conn: State<DbConn>) -> Template {
-    let mut map = HashMap::new();
-    map.insert("title", "Login Page");
-    Template::render("login", map)
+pub struct UserId(i32);
+
+impl<'a, 'r> FromRequest<'a, 'r> for UserId {
+    type Error = !;
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<UserId, !> {
+        request.cookies()
+            .get_private("user_id")
+            .and_then(|cookie| cookie.value().parse().ok())
+            .map(|id| UserId(id))
+            .or_forward(())
+    }
+}
+
+#[get("/login", rank = 2)]
+fn login_page(flash: Option<FlashMessage>) -> Template {
+    let mut context = HashMap::new();
+    if let Some(ref msg) = flash {
+        context.insert("errormessage", msg.msg());
+    } else {
+        context.insert("errormessage", "");
+    }
+
+    Template::render("login", &context)
 }
 
 #[get("/user")]
-pub fn user_page(db_conn: State<DbConn>) -> Template {
+pub fn user_page(db_conn: State<DbConn>, userid: UserId) -> Template {
     let mut map = HashMap::new();
-    map.insert("title", "User Page");
+    let user = get_user_by_id(&db_conn.lock().unwrap(), userid.0).unwrap();
+    map.insert("title", String::from("User Page"));
+    map.insert("name", user.username);
     Template::render("user", map)
 }
 
@@ -45,6 +68,12 @@ pub fn login(db_conn: State<DbConn>, mut cookies: Cookies, user_form: Form<User>
     }
 }
 
+#[post("/logout")]
+pub fn logout(mut cookies: Cookies) -> Redirect {
+    cookies.remove_private(Cookie.named("user_id"));
+    Redirect::to("/login")
+}
+
 #[derive(Debug)]
 struct InvalidLoginError;
 
@@ -56,7 +85,7 @@ impl Display for InvalidLoginError {
     }
 }
 
-pub fn validate(conn: &Connection, user: &User) -> Result<i32, Box<std::error::Error>> {
+fn validate(conn: &Connection, user: &User) -> Result<i32, Box<std::error::Error>> {
     let (id, db_user) = conn.query_row("SELECT id, username, password FROM users WHERE username=?", &[&user.username], |row| {
         let id = row.get(0);
         let username = row.get(1);
@@ -70,4 +99,15 @@ pub fn validate(conn: &Connection, user: &User) -> Result<i32, Box<std::error::E
         true => Ok(id),
         false => Err(Box::new(InvalidLoginError{}))
     }
+}
+
+fn get_user_by_id(conn: &Connection, userid: i32) -> Result<User, rusqlite::Error> {
+    conn.query_row("SELECT username, password FROM users where id=?", &[&userid], |row| {
+        let username = row.get(0);
+        let password = row.get(1);
+        User{
+            username,
+            password
+        }
+    })
 }
