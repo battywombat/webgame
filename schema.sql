1b1faf91c037bf79e10b1e4c83f7fbc36f7938c8
Paul Warner <pauwarne@publicisgroupe.net>
2018-09-06 17:19:16 +0530

    Added sectors table and updated test data.

--- schema.sql
+++ schema.sql
@@ -6,19 +6,6 @@ CREATE TABLE users(
     password CHAR(20)
 );
 
-DROP TABLE IF EXISTS characters;
-CREATE TABLE characters(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    cname VARCHAR(20),
-    strength INTEGER,
-    magic INTEGER,
-    vitality INTEGER,
-    agility INTEGER,
-    luck INTEGER,
-    player INTEGER,
-    FOREIGN KEY(player) REFERENCES users(id)
-);
-
 DROP TABLE IF EXISTS tile_files;
 CREATE TABLE tile_files(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,4 +19,37 @@ CREATE TABLE tiles(
     tile_file INTEGER,
     sub_id INTEGER,
     FOREIGN KEY (tile_file) REFERENCES tile_files(id)
+);
+
+DROP TABLE IF EXISTS sectors;
+CREATE TABLE sectors(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sector_name CHAR(30),
+    width INTEGER,
+    height INTEGER
+);
+
+DROP TABLE IF EXISTS sectordata;
+CREATE TABLE sectordata(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sector INTEGER,
+    sector_sub_id INTEGER,
+    tile INTEGER,
+    FOREIGN KEY(sector) REFERENCES sector(id),
+    FOREIGN KEY(tile) REFERENCES tiles(id)
+);
+
+DROP TABLE IF EXISTS characters;
+CREATE TABLE characters(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cname VARCHAR(20),
+    strength INTEGER,
+    magic INTEGER,
+    vitality INTEGER,
+    agility INTEGER,
+    luck INTEGER,
+    player INTEGER,
+    logged_in INTEGER,
+    FOREIGN KEY(player) REFERENCES users(id),
+    FOREIGN KEY(sector) REFERENCES sectors(id)
 );
--- testdata.sql
+++ testdata.sql
@@ -4,20 +4,23 @@ INSERT INTO users VALUES(
     "pass"
 );
 
-INSERT INTO characters VALUES(
-    1,
-    "Hitoshura",
-    20,
-    15,
-    18,
-    15,
-    11,
-    1
-);
-
 INSERT INTO tile_files (ntiles, path) VALUES(
     140,
     "resources/tiles/PathAndObjects_0.png"
 );
 
 INSERT INTO tiles (tile_file, sub_id) VALUES (1, 0), (1, 1), (1, 2), (1, 3), (1, 4);
+
+INSERT INTO sectors (sector_name, width, height) VALUES ("Test Map", 10, 10);
+
+INSERT INTO characters (cname, strength, magic, vitality, agility, luck, player, logged_in, sector) VALUES(
+    "Hitoshura",
+    20,
+    15,
+    18,
+    15,
+    11,
+    1,
+    0,
+    1
+);
