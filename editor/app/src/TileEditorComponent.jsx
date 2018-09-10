71c5caeaac3bb7ed30973a762492f0410ac8523c
Paul Warner <pauwarne@publicisgroupe.net>
2018-09-10 13:59:13 +0530

    TileEditorComponent can now scroll on right click.

--- editor/app/src/TileEditorComponent.jsx
+++ editor/app/src/TileEditorComponent.jsx
@@ -4,19 +4,68 @@ import styles from './TileEditorComponent.css';
 
 export default class TileEditorComponent extends Component {
 
+    componentWillMount() {
+        this.mouseMoveHandler = this.mouseMove.bind(this);
+        this.img = new Image();
+        this.sx = 0;
+        this.sy = 0;
+        this.sWidth = 0;
+        this.sHeight = 0;
+        this.dWidth = 0;
+        this.dHeight = 0;
+        this.dx = 0;
+        this.dy = 0;
+        this.lastX = 0;
+        this.lastY = 0;
+    }
+
     componentDidUpdate() {
-        let img = new Image();
-        img.src = this.props.src;
-        img.onload = () => {
+        this.img.src = this.props.src;
+        this.img.onload = () => {
             const canvas = this.refs.tileEditorCanvas;
-            const ctx = canvas.getContext('2d');
-            ctx.drawImage(img, 0, 0);
-        }
+            this.sWidth = this.dWidth =  Math.min(this.img.width, canvas.width);
+            this.sHeight = this.dHeight = Math.min(this.img.height, canvas.height);
+            this.redrawImage();
+        };
     }
 
     render() {
         return <div className={styles.TileEditorComponent}>
-            <canvas className={styles.TileEditorCanvas} ref="tileEditorCanvas"></canvas>
+            <canvas onMouseDown={this.mouseDown.bind(this)}
+                    onMouseUp={this.mouseUp.bind(this)}
+                    className={styles.TileEditorCanvas}
+                    ref="tileEditorCanvas"></canvas>
         </div>
+    }
+
+    mouseDown(e) {
+        if (e.button === 2) {
+            this.refs.tileEditorCanvas.addEventListener('mousemove', this.mouseMoveHandler);
+        }
+    }
+
+    mouseUp(e) {
+        if (e.button === 2) {
+            this.refs.tileEditorCanvas.removeEventListener('mousemove', this.mouseMoveHandler);
+        }
+    }
+
+    mouseMove(e) {
+        this.sx -= e.movementX;
+        this.sy -= e.movementY;
+        this.redrawImage();
+    }
+
+    redrawImage() {
+        if (this.img.src !== "") {
+            const canvas = this.refs.tileEditorCanvas;
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(this.img,
+                          this.sx, this.sy,
+                          this.sWidth, this.sHeight,
+                          this.dx, this.dy,
+                          this.dWidth, this.dHeight);
+        }
     }
 }
