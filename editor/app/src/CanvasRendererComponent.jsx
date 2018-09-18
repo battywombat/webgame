7f1f88a6c776d019476dc89f3b765ecfc2115faa
Paul Warner <paul.eliot.warner@gmail.com>
2018-09-18 19:42:59 +0530

    Split CanvasRendererComponent into its own class.

---
+++ editor/app/src/CanvasRendererComponent.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import styles from './CanvasRendererComponent.css';
+
+
+export default class CanvasRendererComponent extends Component {
+
+    componentWillMount() {
+        this.mouseMoveHandler = this.mouseMove.bind(this);
+        this.sx = 0;
+        this.sy = 0;
+        this.sWidth = 0;
+        this.sHeight = 0;
+        this.dWidth = 0;
+        this.dHeight = 0;
+        this.dx = 0;
+        this.dy = 0;
+    }
+
+    componentDidUpdate() {
+        if (this.props.srcImage === undefined) {
+            return;
+        }
+        if (this.props.srcImage.constructor === HTMLImageElement) {
+            onImageLoaded(this.props.srcImage, () => {
+                this.sx = this.sy = 0;
+                const canvas = this.refs.tileEditorCanvas;
+                this.sWidth = this.dWidth =  Math.min(this.props.srcImage.width, canvas.width);
+                this.sHeight = this.dHeight = Math.min(this.props.srcImage.height, canvas.height);
+                this.redrawImage();
+            });
+        }
+    }
+    
+    render() {
+        return <canvas onMouseDown={this.mouseDown.bind(this)}
+                       onMouseUp={this.mouseUp.bind(this)}
+                       onMouseLeave={this.mouseLeave.bind(this)}
+                       className={styles.CanvasRendererComponent}
+                       ref="tileEditorCanvas"></canvas>
+    }
+
+    mouseDown(e) {
+        if (e.button === 2) {
+            this.refs.tileEditorCanvas.addEventListener('mousemove', this.mouseMoveHandler);
+        }
+    }
+
+    mouseUp(e) {
+        if (e.button === 2) {
+            this.refs.tileEditorCanvas.removeEventListener('mousemove', this.mouseMoveHandler);
+        }
+    }
+
+    mouseLeave(e) {
+        if (e.button === 2) {
+            this.refs.tileEditorCanvas.removeEventListener('mousemove', this.mouseMoveHandler);
+        }
+    }
+
+    mouseMove(e) {
+        if (this.props.srcImage !== undefined) {
+            const scrollScale = this.props.scrollScale || 1;
+            const canvas = this.refs.tileEditorCanvas;
+            this.sx = Math.max(Math.min(this.sx - e.movementX*scrollScale, this.props.srcImage.width - canvas.width), 0);
+            this.sy = Math.max(Math.min(this.sy - e.movementY*scrollScale, this.props.srcImage.height - canvas.height), 0);
+            this.redrawImage();
+        }
+    }
+
+    redrawImage() {
+        if (this.props.srcImage !== undefined) {
+            const canvas = this.refs.tileEditorCanvas;
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(this.props.srcImage,
+                          this.sx, this.sy,
+                          this.sWidth, this.sHeight,
+                          this.dx, this.dy,
+                          this.dWidth, this.dHeight);
+        }
+    }
+}
+
+function onImageLoaded(img, cb) {
+    // Create a temporary image, so that if the image is alread loaded, it will be triggered anyway.
+    let testImage = new Image();
+    testImage.src = img.src;
+    testImage.onload = cb;
+}
--- editor/app/src/TileEditorComponent.jsx
+++ editor/app/src/TileEditorComponent.jsx
@@ -1,83 +1,18 @@
 import React, { Component } from 'react';
 
 import styles from './TileEditorComponent.css';
+import CanvasRendererComponent from './CanvasRendererComponent';
 
 export default class TileEditorComponent extends Component {
 
-    componentWillMount() {
-        this.mouseMoveHandler = this.mouseMove.bind(this);
-        this.img = new Image();
-        this.sx = 0;
-        this.sy = 0;
-        this.sWidth = 0;
-        this.sHeight = 0;
-        this.dWidth = 0;
-        this.dHeight = 0;
-        this.dx = 0;
-        this.dy = 0;
-    }
-
-    componentDidUpdate() {
-        if (this.props.src !== undefined) {
-            this.img.src = this.props.src;
-            this.img.onload = () => {
-                this.sx = this.sy = 0;
-                const canvas = this.refs.tileEditorCanvas;
-                this.sWidth = this.dWidth =  Math.min(this.img.width, canvas.width);
-                this.sHeight = this.dHeight = Math.min(this.img.height, canvas.height);
-                this.redrawImage();
-            };
-        }
-    }
-
     render() {
+        const srcImage = new Image();
+        if (this.props.src !== undefined) {
+            srcImage.src = this.props.src;
+        }
         return <div className={styles.TileEditorComponent}>
-            <canvas onMouseDown={this.mouseDown.bind(this)}
-                    onMouseUp={this.mouseUp.bind(this)}
-                    onMouseLeave={this.mouseLeave.bind(this)}
-                    className={styles.TileEditorCanvas}
-                    ref="tileEditorCanvas"></canvas>
+            <CanvasRendererComponent srcImage={srcImage} />
         </div>
     }
 
-    mouseDown(e) {
-        if (e.button === 2) {
-            this.refs.tileEditorCanvas.addEventListener('mousemove', this.mouseMoveHandler);
-        }
-    }
-
-    mouseUp(e) {
-        if (e.button === 2) {
-            this.refs.tileEditorCanvas.removeEventListener('mousemove', this.mouseMoveHandler);
-        }
-    }
-
-    mouseLeave(e) {
-        if (e.button === 2) {
-            this.refs.tileEditorCanvas.removeEventListener('mousemove', this.mouseMoveHandler);
-        }
-    }
-
-    mouseMove(e) {
-        if (this.img.src !== "") {
-            const scrollScale = this.props.scrollScale || 1;
-            const canvas = this.refs.tileEditorCanvas;
-            this.sx = Math.max(Math.min(this.sx - e.movementX*scrollScale, this.img.width - canvas.width), 0);
-            this.sy = Math.max(Math.min(this.sy - e.movementY*scrollScale, this.img.height - canvas.height), 0);
-            this.redrawImage();
-        }
-    }
-
-    redrawImage() {
-        if (this.img.src !== "") {
-            const canvas = this.refs.tileEditorCanvas;
-            const ctx = canvas.getContext('2d');
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.drawImage(this.img,
-                          this.sx, this.sy,
-                          this.sWidth, this.sHeight,
-                          this.dx, this.dy,
-                          this.dWidth, this.dHeight);
-        }
-    }
 }
